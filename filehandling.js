20c0623fa06d515adf5d7cb4f64abdd7aa6a08ab
Faraz-hue <mfarazazam9@gmail.com>
2026-07-16 14:06:01 +0500

    If we read Async it does not return no output is shown

--- filehandling.js
+++ filehandling.js
@@ -4,4 +4,11 @@ const fs = require("fs")
 // fs.writeFileSync("./text.txt", "Hello world")
 
 // ASYNC
-fs.writeFile("./test.docx", "Hello ASYNC That takes a callback fuction as argument", (err) => { })
+// fs.writeFile("./test.docx", "Hello ASYNC That takes a callback fuction as argument", (err) => { })
+
+// Synchronous works weel as it returns 
+// const read = fs.readFileSync("./text.txt", "UTF-8")
+// console.log(read)
+
+// But asynchronous 
+fs.readFile("./test.docx", "UTF-8")
