652c7960704d17903fd65c604eee2dfd9f346f65
Faraz-hue <mfarazazam9@gmail.com>
2026-07-16 13:12:03 +0500

    Synchronus return but Asynchronous does not return

--- filehandling.js
+++ filehandling.js
@@ -1,4 +1,7 @@
 const fs = require("fs")
 
 // Sync
-fs.writeFileSync("./text.txt", "Hello world")
+// fs.writeFileSync("./text.txt", "Hello world")
+
+// ASYNC
+fs.writeFile("./test.docx", "Hello ASYNC That takes a callback fuction as argument", (err) => { })
