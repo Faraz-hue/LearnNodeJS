5595370013fcb08f68991d15beffa837942c641b
Faraz-hue <mfarazazam9@gmail.com>
2026-07-18 10:30:22 +0500

    To display only one user first get id and then find in api/users f id matches then result is hown successful

--- Project-01/index.js
+++ Project-01/index.js
@@ -14,10 +14,18 @@ app.get("/users", (req, res) => {
     </ul>`
     res.send(html)
 })
-
 // 1. REST API points
 app.get("/api/users", (req, res) => {
     return res.json(users)
+})
+
+app.get("/api/users/:id", (req, res) => {
+    // GET ID FIRST
+    const id = Number(req.params.id);
+    // FIND IN JSON Second step
+    const user = users.find((user) => user.id === id
+    )
+    return res.json(user);
 })
 
 
