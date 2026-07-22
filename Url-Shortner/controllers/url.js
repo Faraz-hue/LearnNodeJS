2a3c7c9c54367786ac9116c38cdc53a0c755eca5
Faraz-hue <mfarazazam9@gmail.com>
2026-07-22 20:31:27 +0500

    Completed the short url Generator

--- Url-Shortner/controllers/url.js
+++ Url-Shortner/controllers/url.js
@@ -9,7 +9,7 @@ async function handleGenerateNewShortUrl(req, res) {
         redirectURL: body.url,
         visitHistory: [],
     })
-    return res.json({ id: shortId })
+    return res.render("home", { id: shortId })
 }
 
 async function getHandleAnalytics(req, res) {
